7cd379e8038f6cecd5fd9dd1eb1144edd56ddc84
Robert Rochlin <rrochlin@yahoo.com>
2025-03-19 22:39:32 -0700

    :rocket: feat: added in database integration and better structured config

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,40 @@
-type APIConfig = {
-	fileserverHits: number;
+import { MigrationConfig } from "drizzle-orm/migrator";
+
+process.loadEnvFile()
+
+type Config = {
+	api: APIConfig,
+	db: DBConfig
 }
 
-export const config: APIConfig = {
-	fileserverHits: 0,
+type APIConfig = {
+	fileserverHits: number,
+	port: number,
+	url: string,
+}
+
+type DBConfig = {
+	url: string,
+	migrationConfig: MigrationConfig,
+}
+
+export const config: Config = {
+	api: {
+		fileserverHits: 0,
+		port: Number(envOrThrow("PORT")),
+		url: envOrThrow("HOST_URL")
+	},
+	db: {
+		url: envOrThrow("DB_URL"),
+		migrationConfig: { migrationsFolder: './src/db/migrations' }
+	}
+}
+
+
+
+function envOrThrow(key: string) {
+	if (process.env.hasOwnProperty(key) && process.env[key]) {
+		return process.env[key]
+	}
+	throw `${key} was not set in the env`
 }
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import path from "path";
+import { migrate } from 'drizzle-orm/postgres-js/migrator.js';
+import { drizzle } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
 import { handlerHealth, handlerMetrics, handlerReset, handlerValidateChirp } from "./api/handlers.js"
 import { middlewareLogResponses, middlewareMetricsInc, middlewareHandleError } from "./api/middleware.js"
 import express from "express";
+import { config } from "./config";
 
+const migrationClient = postgres(config.db.url, { max: 1 });
+await migrate(drizzle(migrationClient), config.db.migrationConfig);
 
 const app = express();
-const PORT = 8080
 
 app.use(middlewareLogResponses)
 app.use(express.json())
@@ -17,8 +22,8 @@ app.post('/api/validate_chirp', handlerValidateChirp, middlewareHandleError)
 
 app.use('/app', middlewareMetricsInc, express.static('./src/app'))
 
-app.listen(PORT, '0.0.0.0', () => {
-	console.log(`Server is running at http://localhost:${PORT}`);
+app.listen(config.api.port, config.api.url, () => {
+	console.log(`Server is running at ${config.api.url}:${config.api.port}`);
 });
 
 
